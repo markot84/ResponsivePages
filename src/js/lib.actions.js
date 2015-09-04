506bdd59aa3f5b7ee117b2b60652edd12ae94ebb
Marios Kotoulas <kotoulas.marios@gmail.com>
2015-09-04 17:39:24 +0300

    bug fixes

--- src/js/lib.actions.js
+++ src/js/lib.actions.js
@@ -38,47 +38,47 @@ function create_new_page() {
 	
 	if($('.error').is(":visible") || (!$('#id').val() && !$('#title').val() && !$('#published_on').val())){
 		$('#create_page').removeAttr('disabled');
-	}
-	
-	var data = {
-		id: $('#id').val(),
-		title: $('#title').val(),
-		description: $('#description').val(),
-		type: $('#type').val(),
-		isActive: $('#is_active').prop('checked'),
-		publishedOn: $('#published_on').val(),
-	};
-	
-	data = JSON.stringify(data);
+	}else{
+		show_overlay();
 		
-	$.ajax({
-		type: "POST",
-		contentType: "application/json",
-		url: 'http://pagesmanagement.azurewebsites.net/api/ResponsivePages',
-		data: data,
-		dataType: "json",
-		success: function(data){
-			if(data.id){
-				$.ambiance({
-					message: "Page added successufully",
-					title: "Success!",
-					type: "success"
-				})
-			}else{
-				$.ambiance({
-					message: "An error occured. Please try again",
-					type: "error",
-					title: "Error!"
-				})
+		var data = {
+			id: $('#id').val(),
+			title: $('#title').val(),
+			description: $('#description').val(),
+			type: $('#type').val(),
+			isActive: $('#is_active').prop('checked'),
+			publishedOn: $('#published_on').val(),
+		};
+	
+		data = JSON.stringify(data);
+			
+		$.ajax({
+			type: "POST",
+			contentType: "application/json",
+			url: 'http://pagesmanagement.azurewebsites.net/api/ResponsivePages',
+			data: data,
+			dataType: "json",
+			success: function(data){
+				if(data.id){
+					$.ambiance({
+						message: "Page added successufully",
+						title: "Success!",
+						type: "success"
+					})
+				}else{
+					$.ambiance({
+						message: "An error occured. Please try again",
+						type: "error",
+						title: "Error!"
+					})
+				}
+				window.location.href = "#list_pages/";
 			}
-			window.location.href = "#list_pages/";
-		}
-	});
+		});
+	}
 }
 
 function delete_page(page_id){
-	
-	show_overlay();
 	
 	open_modal({
 		message:'<p>Are you sure you want to delete this page?</p>',
